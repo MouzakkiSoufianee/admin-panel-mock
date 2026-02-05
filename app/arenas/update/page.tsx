6eca64143385159baef90f0ef6f1f1583821b219
Soufiane Mouzakki <s.mouzakki@game-changers.io>
2026-02-05 15:07:06 +0100

    Disable static prerendering for dynamic update page

--- app/arenas/update/page.tsx
+++ app/arenas/update/page.tsx
@@ -1,5 +1,7 @@
 import UpdateArenaPage from '@/app/features/arenas/arenasCrud/UpdateArenaPage';
 
+export const dynamic = 'force-dynamic';
+
 export default function Page() {
   return <UpdateArenaPage />;
 }
